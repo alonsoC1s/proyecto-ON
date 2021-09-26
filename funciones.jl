6daf27c02ca84df5343086e29e03a275afd1197c
Alonso Martinez <alonso.mtz99@gmail.com>
2021-09-26 17:30:34 -0500

    Añadida estructura & linprog

--- funciones.jl
+++ funciones.jl
@@ -1,8 +1,13 @@
 """
 Funciones de apoyo para implementar el algoritmo principal.
 """
+module  Utils
+	
+export rankMethod, linprog
 
 using LinearAlgebra
+using JuMP
+using GLPK
 
 """
 	rankMethod(G, A, c, b)
@@ -35,4 +40,45 @@ function rankMethod(G, A, c, b)
 	x_star = (-Ginv * c) - (Ginv * A' * λ)
 
 	return x_star
+end
+
+
+"""
+	linprog(A, b_E, b_I)
+
+Envuelve JuMP y la rutina de simplex para devolver un punto factible al un problema 
+cuadrático con restricciones de desigualdad con `m` restricciones y `n` variables.
+
+Resuelve:
+	min sum(ones(n))
+	s.a A * x = b_E
+		A * x <= b_I
+
+# Arguments
+- `A::Matrix(m, n)`: La matriz de restricciones del problema. 
+- `b_E::Vector(n_e)`: Vector de restricciones de igualdad.
+- `b_I::Vector(n_i)`: Vector de restricciones de desigualdad.
+n = n_e + n_i
+"""
+function linprog(A, b_E, b_I)
+	# Guardando número de igualdades & desigualdades
+	n_e = length(b_E)
+	n_i = length(b_I)
+
+	# Inicializando modelo
+	model = Model(GLPK.Optimizer)
+
+	# Agregando n variables al modelo
+	@variable(model, x[1:size(A,1)])
+
+	# Agregando restricciones de igualdad y desigualdad
+	@constraint(model, con, A[1:n_e, :] * x[1:n_e] .== b_E)
+	@constraint(model, con, A[1:n_i, :] * x[1:n_i] .<= b_I)
+
+	optimize!(model)
+
+	# Regresando el valor óptimo
+	return value.(x)
+end
+
 end
---
+++ main.jl
@@ -0,0 +1,17 @@
+"""
+Implementacion principal del metodo de conjunto activo
+"""
+
+# Incluyendo y usando el módulo de utils definido en el archivo funcines.jl
+include("funciones.jl")
+using .Utils
+
+function rama1()
+end
+
+function rama2()
+end
+
+function activeSetMethod()
+	
+end
